8cfeb7ab09e5f563f40288b227934c79f07ed060
MasterCheapster <84666759+MasterCheapster@users.noreply.github.com>
2023-05-25 11:00:57 +0530

    Tv show list item

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,77 @@
-export function App(){
-    return <div>ayush</div>
+import { useEffect, useState } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import s from "./style.module.css";
+import { BACKDROP_BASE_URL } from "./config";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { Logo } from "./components/Logo/Logo";
+import logoImg from "./assets/images/logo.png";
+import { TVShowListItem } from "./components/TVShowListItem/TVShowListItem";
+
+export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const popularTVShowList = await TVShowAPI.fetchPopulars();
+    if (popularTVShowList.length > 0) {
+      setCurrentTVShow(popularTVShowList[0]);
+    }
+  }
+
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  return (
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)),
+             url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <Logo
+              title="Watowatch"
+              subtitle="Find a show you may like"
+              image={logoImg}
+            />
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_details}>
+        {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+      </div>
+      <div className={s.recommended_shows}>
+        {currentTVShow && (
+          <>
+            <TVShowListItem
+              tvShow={currentTVShow}
+              onClick={(tvShow) => {
+                console.log("i have been clicked", tvShow);
+              }}
+            />
+            <TVShowListItem
+              tvShow={currentTVShow}
+              onClick={(tvShow) => {
+                console.log("i have been clicked", tvShow);
+              }}
+            />
+            <TVShowListItem
+              tvShow={currentTVShow}
+              onClick={(tvShow) => {
+                console.log("i have been clicked", tvShow);
+              }}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
